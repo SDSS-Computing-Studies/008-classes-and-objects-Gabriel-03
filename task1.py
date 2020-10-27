ff2ef516b97a0bfffbc5f3d10aae74fcd0d3368a
Gabriel-03 <71287702+Gabriel-03@users.noreply.github.com>
2020-10-27 12:33:11 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,7 +8,6 @@ str studentNumber
 int grade
 list courses - to corresepond with course names
 list grade - to correspond with grades
-
 It should have the following methods:
 average()       - determines the mathematical average of all course grades
 getHonorRoll()  - determines the average of the top 5 courses if there are at least 5 courses.
@@ -21,19 +20,32 @@ getCourses(list)- Receives a list of courses and stores that in the class proper
 getGrades(list) - Receives a list of grades and stores that in the class property
 constructor     - should require the student name, studentNumber and grade (in that order)
 """
-
 class student:
 
     # properties should be listed first
+    name = ""
+    code = ""
+    grade = int()
+    courses = []
+    grades = []
 
-    def __init__(): # You will need to create your own input parameters for all methods
-        pass
+    def getCourses(self, lis):
+        self.courses = lis
 
-    def __del__():
-        pass
+    def getGrades(self, *arg):
+        gr = []
+        for x in range(len(arg)):
+            gr.append(arg[x])
+        self.grades = gr
 
-    def average(self):
-        pass
+    def __init__(self, name, code, grade): # You will need to create your own input parameters for all methods
+        self.name = name
+        self.code = code
+        self.grade = grade
+
+    def __del__(self):
+        x = print("Thank you for viewing the profile of " + self.name)
+
 
 def main():
     # This contains test data that will be used by the autograder.
@@ -41,13 +53,11 @@ def main():
 
     st1 = student("Anita Bath","91334",11)
     st1.getCourses( ["English","Math","PE","Computers","History","Biology","Japanese"] )
-    st1.getGrades( [91, 94, 87, 99, 82, 100, 73])
+    st1.getGrades([91, 94, 87, 99, 82, 100, 73])
 
     st2 = student("Joe Lunchbox","12346", 11)
     st1.getCourses( ["English","Math","Physics","Computers","Geography","Chemistry","French"] )
     st1.getGrades( [71, 98, 93, 95, 68, 81, 71])
-
-
 
 
 main()
